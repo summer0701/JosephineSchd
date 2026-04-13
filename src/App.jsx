e189d22cfbe68dffe783a2e0e4809a1f9a59cc92
summer0701 <garts22@gmail.com>
2026-04-13 17:24:10 +0900

    이름 입력

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [name, setName] = useState('');
+
+  return (
+    <div>
+      <h1>Name Input App</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name"
+      />
+      <p>Hello, {name}!</p>
+    </div>
+  );
+}
+
+export default App;
